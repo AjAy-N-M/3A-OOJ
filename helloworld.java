5762c3226901d91ddeb995c4a793cf14a1f7a698
AjAy-N-M <153304670+AjAy-N-M@users.noreply.github.com>
2023-12-10 22:44:05 +0530

    Delete helloworld.java

--- helloworld.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-	public class helloworld{
-		public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
-		System.out.print("enter a new number : ");
-		int number = reader.nextInt();
-		System.out.println("you entered : "+ number);
-	}
-}
